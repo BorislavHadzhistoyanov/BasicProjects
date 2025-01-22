976bea3d0ba17192f533c8f8dae2059c06849c6e
BorislavHadzhistoyanov <boby2005gh@gmail.com>
2025-01-22 21:07:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string passengerName = "Zara";
+int passengerAge = 28;
+string ticketType = "First Class";
+string prefferedPlanet = "Mars";
+
+passengerAge = passengerAge + 1;
+
+Console.WriteLine(passengerName);
+Console.WriteLine(passengerAge);
+Console.WriteLine(ticketType);
+Console.WriteLine(prefferedPlanet);
+
+double passengerAgeDouble = (double)passengerAge;
+Console.WriteLine(passengerAgeDouble);
+
+double passengerAgeDouble2 = passengerAge;
+Console.WriteLine(passengerAgeDouble2);
+
+string passengerAgeString = Convert.ToString(passengerAge);
+Console.WriteLine(passengerAgeString);
