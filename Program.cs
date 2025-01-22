ea557019f2c5eb8ca1696a96d8c2c1962479acb7
BorislavHadzhistoyanov <boby2005gh@gmail.com>
2025-01-22 21:49:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double area = double.Parse(Console.ReadLine());
+
+double areaPrice = area * 7.61;
+double newAreaPrice = areaPrice * 0.82;
+
+Console.WriteLine($"The final price is: {newAreaPrice} lv.");
+Console.WriteLine($"The discount is: {areaPrice - newAreaPrice} lv.");
